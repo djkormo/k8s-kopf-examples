e1a7cf9bbd8715709e0aaf6961fbacb0af932a27
djkormo <djkormo@gmail.com>
2021-07-18 14:33:11 +0200

    first version of create namespace

---
+++ kopf-example-project-operator/app/on_create.py
@@ -0,0 +1,60 @@
+import kopf
+import kubernetes
+import yaml
+import os
+from kubernetes.client.rest import ApiException
+from pprint import pprint
+
+@kopf.on.create('djkormo.github', 'v1alpha1', 'project')
+def create_fn(spec, name, namespace, logger, **kwargs):
+    print(f"Creating: {spec}")
+    resourcequotarequestscpu = spec.get('resourcequotarequestscpu',1)
+    if not resourcequotarequestscpu:
+      raise kopf.PermanentError(f"resourcequotarequestscpu must be set. Got {resourcequotarequestscpu!r}.")
+
+    resourcequotarequestsmemory = spec.get('resourcequotarequestsmemory',1)
+    if not resourcequotarequestsmemory:
+      raise kopf.PermanentError(f"resourcequotarequestsmemory must be set. Got {resourcequotarequestsmemory!r}.")
+    
+
+    api = kubernetes.client.CoreV1Api()
+
+     # get context of yaml manifest for namespace
+
+    path = os.path.join(os.path.dirname(__file__), 'namespace.yaml')
+    tmpl = open(path, 'rt').read()
+    text = tmpl.format(name=name, namespace=namespace,
+    )
+    data = yaml.safe_load(text)
+    ## Create namespace
+    try:
+    
+      obj = api.create_namespace(
+          body=data,
+      )
+      pprint(obj)
+    except ApiException as e:
+      print("Exception when calling CoreV1Api->create_namespace: %s\n" % e)  
+    
+    # get context of yaml manifest for resource quota
+
+    path = os.path.join(os.path.dirname(__file__), 'resourcequota.yaml')
+    tmpl = open(path, 'rt').read()
+    text = tmpl.format(name=name, resourcequotarequestscpu=resourcequotarequestscpu,
+           resourcequotarequestsmemory=resourcequotarequestsmemory 
+    )
+    
+    data = yaml.safe_load(text)
+    
+    # Create resourcequota
+    try:
+      obj = api.create_namespaced_resource_quota(
+          namespace=namespace,
+          body=data,
+      )
+      pprint(obj)
+    except ApiException as e:
+      print("Exception when calling CoreV1Api->create_namespaced_resource_quota: %s\n" % e)  
+    return {'message': 'done'}  # will be the new status
+
+
