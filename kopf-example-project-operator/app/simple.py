a1e76a75cfb9d65411ffd93d4f6de8a7c9e73d31
djkormo <djkormo@gmail.com>
2021-07-18 14:02:08 +0200

    correcting crd

--- kopf-example-project-operator/app/simple.py
+++ kopf-example-project-operator/app/simple.py
@@ -3,7 +3,7 @@ import kubernetes
 import yaml
 import os
 
-@kopf.on.create('djkormo', 'v1alpha1', 'project')
+@kopf.on.create('djkormo.github', 'v1alpha1', 'project')
 def create_fn(spec, **kwargs):
     print(f"Creating: {spec}")
     return {'message': 'done'}  # will be the new status
