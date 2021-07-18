0c14b6fe9d478c03ab0e5857390d7197b184ed92
djkormo <djkormo@gmail.com>
2021-07-18 15:21:16 +0200

    adding more fields in spec

--- kopf-example-project-operator/app/on_create.py
+++ kopf-example-project-operator/app/on_create.py
@@ -8,6 +8,7 @@ from pprint import pprint
 @kopf.on.create('djkormo.github', 'v1alpha1', 'project')
 def create_fn(spec, name, namespace, logger, **kwargs):
     print(f"Creating: {spec}")
+    logger.info(f"Object is created: {spec}")
     resourcequotarequestscpu = spec.get('resourcequotarequestscpu')
     if not resourcequotarequestscpu:
       raise kopf.PermanentError(f"resourcequotarequestscpu must be set. Got {resourcequotarequestscpu!r}.")
@@ -16,6 +17,17 @@ def create_fn(spec, name, namespace, logger, **kwargs):
     if not resourcequotarequestsmemory:
       raise kopf.PermanentError(f"resourcequotarequestsmemory must be set. Got {resourcequotarequestsmemory!r}.")
     
+    resourcequotalimitscpu=spec.get('resourcequotalimitscpu')
+    resourcequotalimitsmemory=spec.get('resourcequotalimitsmemory')
+    resourcequotacountjobsbatch=spec.get('resourcequotacountjobsbatch')
+    resourcequotacountingresses=spec.get('resourcequotacountingresses')
+    resourcequotapods=spec.get('resourcequotapods')
+    resourcequotaservices=spec.get('resourcequotaservices')
+    resourcequotaconfigmaps=spec.get('resourcequotaconfigmaps')
+    resourcequotapersistentvolumeclaims=spec.get('resourcequotapersistentvolumeclaims'),
+    resourcequotareplicationcontrollers=spec.get('resourcequotareplicationcontrollers')
+    resourcequotasecrets=spec.get('resourcequotasecrets')
+    resourcequotaservicesloadbalancers=spec.get('resourcequotaservicesloadbalancers')
 
     api = kubernetes.client.CoreV1Api()
 
@@ -23,7 +35,7 @@ def create_fn(spec, name, namespace, logger, **kwargs):
 
     path = os.path.join(os.path.dirname(__file__), 'namespace.yaml')
     tmpl = open(path, 'rt').read()
-    text = tmpl.format(name=name, namespace=namespace,
+    text = tmpl.format(name=name, namespace=namespace
     )
     data = yaml.safe_load(text)
 
@@ -43,6 +55,17 @@ def create_fn(spec, name, namespace, logger, **kwargs):
     tmpl = open(path, 'rt').read()
     text = tmpl.format(name=name, resourcequotarequestscpu=resourcequotarequestscpu,
            resourcequotarequestsmemory=resourcequotarequestsmemory, 
+           resourcequotalimitscpu=resourcequotalimitscpu,
+           resourcequotalimitsmemory=resourcequotalimitsmemory,
+           resourcequotacountjobsbatch=resourcequotacountjobsbatch,
+           resourcequotacountingresses=resourcequotacountingresses,
+           resourcequotapods=resourcequotapods,
+           resourcequotaservices=resourcequotaservices,
+           resourcequotaconfigmaps=resourcequotaconfigmaps,
+           resourcequotapersistentvolumeclaims=resourcequotapersistentvolumeclaims,
+           resourcequotareplicationcontrollers=resourcequotareplicationcontrollers,
+           resourcequotasecrets=resourcequotasecrets,
+           resourcequotaservicesloadbalancers=resourcequotaservicesloadbalancers
     )
     
     data = yaml.safe_load(text)
@@ -58,7 +81,7 @@ def create_fn(spec, name, namespace, logger, **kwargs):
       logger.info(f"ResourceQuota child is created: {obj}")
     except ApiException as e:
       print("Exception when calling CoreV1Api->create_namespaced_resource_quota: %s\n" % e)  
-      
+
     return {'message': 'done'}  # will be the new status
 
 
