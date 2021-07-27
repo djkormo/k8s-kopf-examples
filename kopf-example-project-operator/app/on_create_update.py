c6b212ef971f699cd9309cc88af161674041fc06
djkormo <djkormo@gmail.com>
2021-07-27 17:13:02 +0200

    adding limitrange

--- kopf-example-project-operator/app/on_create_update.py
+++ kopf-example-project-operator/app/on_create_update.py
@@ -88,9 +88,7 @@ def create_fn(spec, name, namespace, logger, **kwargs):
     kopf.adopt(data)
 
 
-    # Create resourcequota
-
-  #  kopf.adjust_namespace(obj,name, forced=True)
+  # Create resourcequota
 
     try:
       obj = api.create_namespaced_resource_quota(
@@ -102,20 +100,91 @@ def create_fn(spec, name, namespace, logger, **kwargs):
     except ApiException as e:
       print("Exception when calling CoreV1Api->create_namespaced_resource_quota: %s\n" % e)  
 
+    # create limitrange 
+    # get context of yaml manifest for resource quota
+
+    path = os.path.join(os.path.dirname(__file__), 'limitrange.yaml')
+    tmpl = open(path, 'rt').read()
+    limitrangemaxcpu = spec.get('limitrangemaxcpu',1)
+    limitrangemaxmem = spec.get('limitrangemaxmem',1)
+    limitrangemincpu = spec.get('limitrangemincpu',1)
+    limitrangeminmem = spec.get('limitrangeminmem',1)
+    limitrangedefaultcpu = spec.get('limitrangedefaultcpu',1)
+    limitrangedefaultmem = spec.get('limitrangedefaultmem',1)
+    limitrangedefaultrequestcpu = spec.get('limitrangedefaultrequestcpu',1)
+    limitrangedefaultrequestmem = spec.get('limitrangedefaultrequestmem',1)
+    text = tmpl.format(name=name,limitrangemaxmem=limitrangemaxmem,
+           limitrangemaxcpu=limitrangemaxcpu, 
+           limitrangemincpu=limitrangemincpu,
+           limitrangeminmem=limitrangeminmem,
+           limitrangedefaultcpu=limitrangedefaultcpu,
+           limitrangedefaultmem=limitrangedefaultmem,
+           limitrangedefaultrequestcpu=limitrangedefaultrequestcpu,
+           limitrangedefaultrequestmem=limitrangedefaultrequestmem,
+    )
+    
+    data = yaml.safe_load(text)
+    try:
+      obj = api.create_namespaced_limit_range(
+          namespace=name,
+          body=data,
+      )
+      pprint(obj)
+      logger.info(f"LimitRange child is created: {obj}")
+    except ApiException as e:
+      print("Exception when calling CoreV1Api->create_namespaced_limit_range: %s\n" % e)
+    
     return {'project-name': obj.metadata.name}
 
 
 @kopf.on.update('djkormo.github', 'v1alpha1', 'project')
-def update_fn(spec, name, status, namespace, logger, **kwargs):
+def update_fn(spec, name, status, namespace, logger,diff, **kwargs):
     print(f"Updating: {spec}")
 
-    resourcequotarequestscpu = spec.get('resourcequotarequestscpu',1)
+    resourcequotarequestscpu = spec.get('resourcequotarequestscpu',None)
     if not resourcequotarequestscpu:
       raise kopf.PermanentError(f"resourcequotarequestscpu must be set. Got {resourcequotarequestscpu!r}.")
 
+    resourcequotarequestsmemory = spec.get('resourcequotarequestsmemory',1)
+    if not resourcequotarequestsmemory:
+      raise kopf.PermanentError(f"resourcequotarequestsmemory must be set. Got {resourcequotarequestsmemory!r}.")
+    resourcequotalimitscpu=spec.get('resourcequotalimitscpu',1)
+    resourcequotalimitsmemory=spec.get('resourcequotalimitsmemory',1)
+    resourcequotacountjobsbatch=spec.get('resourcequotacountjobsbatch',1)
+    resourcequotacountingresses=spec.get('resourcequotacountingresses',1)
+    resourcequotapods=spec.get('resourcequotapods',1)
+    resourcequotaservices=spec.get('resourcequotaservices',1)
+    resourcequotaconfigmaps=spec.get('resourcequotaconfigmaps',1)
+    resourcequotapersistentvolumeclaims=spec.get('resourcequotapersistentvolumeclaims',1),
+    resourcequotareplicationcontrollers=spec.get('resourcequotareplicationcontrollers',1)
+    resourcequotasecrets=spec.get('resourcequotasecrets',1)
+    resourcequotaservicesloadbalancers=spec.get('resourcequotaservicesloadbalancers',1)
 
     project_name = status['create_fn']['project-name']
-    project_patch = {'spec': {'hard': {'requests.cpu': resourcequotarequestscpu}}}
+
+    path = os.path.join(os.path.dirname(__file__), 'resourcequota.yaml')
+    tmpl = open(path, 'rt').read()
+    text = tmpl.format(name=name,resourcequotarequestscpu=resourcequotarequestscpu,
+           resourcequotarequestsmemory=resourcequotarequestsmemory, 
+           resourcequotalimitscpu=resourcequotalimitscpu,
+           resourcequotalimitsmemory=resourcequotalimitsmemory,
+           resourcequotacountjobsbatch=resourcequotacountjobsbatch,
+           resourcequotacountingresses=resourcequotacountingresses,
+           resourcequotapods=resourcequotapods,
+           resourcequotaservices=resourcequotaservices,
+           resourcequotaconfigmaps=resourcequotaconfigmaps,
+           resourcequotapersistentvolumeclaims=resourcequotapersistentvolumeclaims,
+           resourcequotareplicationcontrollers=resourcequotareplicationcontrollers,
+           resourcequotasecrets=resourcequotasecrets,
+           resourcequotaservicesloadbalancers=resourcequotaservicesloadbalancers
+    )
+    data = yaml.safe_load(text)
+    
+    logger.info(f"ResourceQuota child definition: {data}")
+
+    kopf.adopt(data)
+
+    #project_patch = {'spec': {'hard': {'requests.cpu': resourcequotarequestscpu}}}
     logger.info(f"Object project is updated: {spec}")
     api = kubernetes.client.CoreV1Api()
 
@@ -123,7 +192,7 @@ def update_fn(spec, name, status, namespace, logger, **kwargs):
       obj = api.patch_namespaced_resource_quota(
           namespace=name,
           name=project_name,
-          body=project_patch,
+          body=data,
       )
       pprint(obj)
       logger.info(f"ResourceQuota child is updated: {obj}")
@@ -132,29 +201,29 @@ def update_fn(spec, name, status, namespace, logger, **kwargs):
     
     return {'project-name': obj.metadata.name}
     
-@kopf.on.field('djkormo.github', 'v1alpha1', 'project', field='spec.hard')
-def relabel(diff, status,name, namespace, logger, **kwargs):
-    project_patch = {field[0]: new for op, field, old, new in diff}
-    project_name = status['relabel']['project-name']
-    project_path = {'spec': {'hard': project_patch}}
+#@kopf.on.field('djkormo.github', 'v1alpha1', 'project', field='spec.hard')
+#def relabel(diff, status,name, namespace, logger, **kwargs):
+#    project_patch = {field[0]: new for op, field, old, new in diff}
+#    project_name = status['relabel']['project-name']
+#    project_path = {'spec': {'hard': project_patch}}
 
-    logger.info(f"Object project is updated: {project_path}")
+#    logger.info(f"Object project is updated: {project_path}")
 
-    api = kubernetes.client.CoreV1Api()
+#    api = kubernetes.client.CoreV1Api()
 
-    try:
-      obj = api.patch_namespaced_resource_quota(
-          namespace=name,
-          name=project_name,
-          body=project_patch,
-      )
-      pprint(obj)
-      logger.info(f"ResourceQuota child is updated: {obj}")
-    except ApiException as e:
-      print("Exception when calling CoreV1Api->patch_namespaced_resource_quota: %s\n" % e)  
+#    try:
+#      obj = api.patch_namespaced_resource_quota(
+#          namespace=name,
+#          name=project_name,
+#          body=project_patch,
+#      )
+#      pprint(obj)
+#      logger.info(f"ResourceQuota child is updated: {obj}")
+#    except ApiException as e:
+#      print("Exception when calling CoreV1Api->patch_namespaced_resource_quota: %s\n" % e)  
 
-    return {'project-name': obj.metadata.name}
+ #   return {'project-name': obj.metadata.name}
 
 @kopf.on.delete('djkormo.github', 'v1alpha1', 'project')
-def update_fn(spec, name, status, namespace, logger, **kwargs):
+def delete_fn(spec, name, status, namespace, logger, **kwargs):
     print(f"Deleting: {spec}")
