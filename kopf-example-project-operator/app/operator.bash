b2de8e4ef34431fab221a24e7c7820fbc10c35ee
djkormo <djkormo@gmail.com>
2021-07-29 16:07:12 +0200

    first env version via bash 10

--- kopf-example-project-operator/app/operator.bash
+++ kopf-example-project-operator/app/operator.bash
@@ -5,14 +5,14 @@ command="kopf run --standalone /home/worker/app/operator.py "
 #[[ "$VERBOSE" = "true" ]] && command+=("--verbose")
 
 if [[ "$VERBOSE" -eq "true" ]]; then
-    echo "$VERBOSE is set true"
-    $command= "$command --verbose"
+    echo "VERBOSE is set true"
+    command= "$command --verbose"
   fi
 
 #[[ "$DEBUG" = "true" ]] && command+=("--debug")
 if [[ "$DEBUG" -eq "true" ]]; then
-    echo "$DEBUG is set true"
-    $command= "$command --debug"
+    echo "DEBUG is set true"
+    command= "$command --debug"
 fi
 
 #[ -n "$NAMESPACE" ] && [ "$NAMESPACE" != "ALL" ] && echo "Only watching resources from the ${NAMESPACE} namespace" && command+=("--namespace=${NAMESPACE}")
@@ -20,8 +20,8 @@ fi
 #[[ "$LIVENESS" = "true" ]] && echo "Liveness /healthz endpoint has been explicitely enabled!" || command+=("--liveness=http://0.0.0.0:8080/healthz")
 
 if [[ "$LIVENESS" -eq "true" ]]; then
-    echo "$LIVENESS is set true"
-    $command= "$command --liveness=http://0.0.0.0:8080/healthz"
+    echo "LIVENESS is set true"
+    command= "$command --liveness=http://0.0.0.0:8080/healthz"
 fi
 
 USER=$(id -u)
