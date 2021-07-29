8bc86b3def841764b120aaf9447e8f05deaca6dc
djkormo <djkormo@gmail.com>
2021-07-29 15:38:38 +0200

    first env version via bash 6

--- kopf-example-project-operator/app/operator.bash
+++ kopf-example-project-operator/app/operator.bash
@@ -2,10 +2,24 @@
 set -e
 set -u
 command="kopf run --standalone /home/worker/app/operator.py "
-[[ "$VERBOSE" = "true" ]] && command+=("--verbose")
-[[ "$DEBUG" = "true" ]] && command+=("--debug")
-[ -n "$NAMESPACE" ] && [ "$NAMESPACE" != "ALL" ] && echo "Only watching resources from the ${NAMESPACE} namespace" && command+=("--namespace=${NAMESPACE}")
-[[ "$LIVENESS" = "true" ]] && echo "Liveness /healthz endpoint has been explicitely enabled!" || command+=("--liveness=http://0.0.0.0:8080/healthz")
+#[[ "$VERBOSE" = "true" ]] && command+=("--verbose")
+
+if [[ "$VERBOSE" -eq "true" ]]; then
+    command+= " --verbose"
+  fi
+
+#[[ "$DEBUG" = "true" ]] && command+=("--debug")
+if [[ "$DEBUG" -eq "true" ]]; then
+    command+=" --debug"
+fi
+
+#[ -n "$NAMESPACE" ] && [ "$NAMESPACE" != "ALL" ] && echo "Only watching resources from the ${NAMESPACE} namespace" && command+=("--namespace=${NAMESPACE}")
+
+#[[ "$LIVENESS" = "true" ]] && echo "Liveness /healthz endpoint has been explicitely enabled!" || command+=("--liveness=http://0.0.0.0:8080/healthz")
+
+if [[ "$LIVENESS" -eq "true" ]]; then
+    command+=" --liveness=http://0.0.0.0:8080/healthz"
+fi
 
 USER=$(id -u)
 echo "Setting USER environment variable to ${USER}"
