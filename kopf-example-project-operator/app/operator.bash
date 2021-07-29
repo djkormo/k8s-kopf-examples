f3940f3ec71f8b520decd3bb71ffda7a52d7702d
djkormo <djkormo@gmail.com>
2021-07-29 15:02:45 +0200

    first env version via bash 2

--- kopf-example-project-operator/app/operator.bash
+++ kopf-example-project-operator/app/operator.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 set -u
-command=("kopf" "run" "--standalone" "/home/worker/app/operator.bash")
+command=("kopf" "run" "--standalone" "/home/worker/app/operator.py")
 [[ "$VERBOSE" = "true" ]] && command+=("--verbose")
 [[ "$DEBUG" = "true" ]] && command+=("--debug")
 [ -n "$NAMESPACE" ] && [ "$NAMESPACE" != "ALL" ] && echo "Only watching resources from the ${NAMESPACE} namespace" && command+=("--namespace=${NAMESPACE}")
