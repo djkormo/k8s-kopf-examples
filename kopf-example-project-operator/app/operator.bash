d2f2cd7695d9734fca7b2d5097ea6e38f3835360
djkormo <djkormo@gmail.com>
2021-07-29 15:26:07 +0200

    first env version via bash 5

--- kopf-example-project-operator/app/operator.bash
+++ kopf-example-project-operator/app/operator.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 set -u
-command=("kopf" "run" "--standalone" "/home/worker/app/operator.py")
+command="kopf run --standalone /home/worker/app/operator.py "
 [[ "$VERBOSE" = "true" ]] && command+=("--verbose")
 [[ "$DEBUG" = "true" ]] && command+=("--debug")
 [ -n "$NAMESPACE" ] && [ "$NAMESPACE" != "ALL" ] && echo "Only watching resources from the ${NAMESPACE} namespace" && command+=("--namespace=${NAMESPACE}")
-[[ "$LIVENESS" = "false" ]] && echo "Liveness /healthz endpoint has been explicitely disabled!" || command+=("--liveness=http://0.0.0.0:8080/healthz")
+[[ "$LIVENESS" = "true" ]] && echo "Liveness /healthz endpoint has been explicitely enabled!" || command+=("--liveness=http://0.0.0.0:8080/healthz")
 
 USER=$(id -u)
 echo "Setting USER environment variable to ${USER}"
