19b92782f1c29672196c5f49d0b1d7c30635ff45
djkormo <djkormo@gmail.com>
2021-08-05 14:02:46 +0200

    adding limitrange

--- kopf-example-limitrange-np-operator/app/minimal.py
+++ kopf-example-limitrange-np-operator/app/minimal.py
@@ -23,6 +23,46 @@ def get_random_value(**kwargs):
 def create_fn(spec, name, namespace, logger, **kwargs):
     print(f"Creating: {spec}")
 
+    api = kubernetes.client.CoreV1Api()
+
+    # create limitrange 
+    # get context of yaml manifest for limitrange
+
+    path = os.path.join(os.path.dirname(__file__), 'limitrange.yaml')
+    tmpl = open(path, 'rt').read()
+    limitrangemaxcpu = spec.get('limitrangemaxcpu',"20000m")
+    limitrangemaxmem = spec.get('limitrangemaxmem',"30Gi")
+    limitrangemincpu = spec.get('limitrangemincpu',"50m")
+    limitrangeminmem = spec.get('limitrangeminmem',"50Mi")
+    limitrangedefaultcpu = spec.get('limitrangedefaultcpu',"1000m")
+    limitrangedefaultmem = spec.get('limitrangedefaultmem',"1000Mi")
+    limitrangedefaultrequestcpu = spec.get('limitrangedefaultrequestcpu',"100m")
+    limitrangedefaultrequestmem = spec.get('limitrangedefaultrequestmem',"100Mi")
+    
+    text = tmpl.format(name=name,limitrangemaxmem=limitrangemaxmem,
+           limitrangemaxcpu=limitrangemaxcpu, 
+           limitrangemincpu=limitrangemincpu,
+           limitrangeminmem=limitrangeminmem,
+           limitrangedefaultcpu=limitrangedefaultcpu,
+           limitrangedefaultmem=limitrangedefaultmem,
+           limitrangedefaultrequestcpu=limitrangedefaultrequestcpu,
+           limitrangedefaultrequestmem=limitrangedefaultrequestmem,
+    )
+
+    data = yaml.safe_load(text)
+    try:
+      obj = api.create_namespaced_limit_range(
+          namespace=name,
+          body=data,
+      )
+      #pprint(obj)
+      kopf.append_owner_reference(obj)
+      logger.info(f"LimitRange child is created: {obj}")
+    except ApiException as e:
+      print("Exception when calling CoreV1Api->create_namespaced_limit_range: %s\n" % e)
+    kopf.adopt(data)
+
+
 # When updating object
 @kopf.on.update('namespace')
 def update_fn(spec, name, status, namespace, logger,diff, **kwargs):
