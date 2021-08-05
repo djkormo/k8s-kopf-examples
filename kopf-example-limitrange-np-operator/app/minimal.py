a388beca3ed77d124d864ec29ac38532aaf39c1b
djkormo <djkormo@gmail.com>
2021-08-05 14:50:18 +0200

    adding netpol 5

--- kopf-example-limitrange-np-operator/app/minimal.py
+++ kopf-example-limitrange-np-operator/app/minimal.py
@@ -61,18 +61,16 @@ def create_fn(spec, name, namespace, logger, **kwargs):
     except ApiException as e:
       print("Exception when calling CoreV1Api->create_namespaced_limit_range: %s\n" % e)
     kopf.adopt(data)
-
+    return {'project-name': obj.metadata.name}
     # create network policy
 
     api = kubernetes.client.NetworkingV1Api()
+
     path = os.path.join(os.path.dirname(__file__), 'networkpolicy-allow-dns-access.yaml')
     tmpl = open(path, 'rt').read()
     pprint(tmpl)
-    #text = tmpl.format(namespace=namespace)
-    
     data = yaml.safe_load(tmpl)
 
-    pprint(data)
     try:
       obj = api.create_namespaced_network_policy(
           namespace=name,
@@ -86,6 +84,46 @@ def create_fn(spec, name, namespace, logger, **kwargs):
     kopf.adopt(data)
 
     return {'project-name': obj.metadata.name}
+
+    path = os.path.join(os.path.dirname(__file__), 'networkpolicy-default-deny-ingress.yaml')
+    tmpl = open(path, 'rt').read()
+    pprint(tmpl)
+    data = yaml.safe_load(tmpl)
+
+    try:
+      obj = api.create_namespaced_network_policy(
+          namespace=name,
+          body=data,
+      )
+      pprint(obj)
+      logger.info(f"NetworkPolicy child is created: {obj}")
+    except ApiException as e:
+      print("Exception when calling NetworkingV1Api->create_namespaced_network_policy: %s\n" % e)
+    
+    kopf.adopt(data)
+
+    return {'project-name': obj.metadata.name}
+
+
+    path = os.path.join(os.path.dirname(__file__), 'networkpolicy-default-deny-egress.yaml')
+    tmpl = open(path, 'rt').read()
+    pprint(tmpl)
+    data = yaml.safe_load(tmpl)
+
+    try:
+      obj = api.create_namespaced_network_policy(
+          namespace=name,
+          body=data,
+      )
+      pprint(obj)
+      logger.info(f"NetworkPolicy child is created: {obj}")
+    except ApiException as e:
+      print("Exception when calling NetworkingV1Api->create_namespaced_network_policy: %s\n" % e)
+    
+    kopf.adopt(data)
+
+    return {'project-name': obj.metadata.name}
+
 
 # When updating object
 @kopf.on.update('namespace')
