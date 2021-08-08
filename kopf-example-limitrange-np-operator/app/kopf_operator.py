695d1bb224e820f9108de88927a79e6cb5cd2936
KP <djkormo@gmail.com>
2021-08-08 21:37:00 +0200

    testing existing objects in cluster

--- kopf-example-limitrange-np-operator/app/kopf_operator.py
+++ kopf-example-limitrange-np-operator/app/kopf_operator.py
@@ -137,15 +137,35 @@ def create_fn(spec, name, namespace, logger, **kwargs):
 
     # create limitrange 
     api = kubernetes.client.CoreV1Api()
+
+    try: 
+      api_response = api.list_namespaced_limit_range(namespace=name) #, pretty=pretty, field_selector=field_selector, include_uninitialized=include_uninitialized, label_selector=label_selector, resource_version=resource_version, timeout_seconds=timeout_seconds, watch=watch)
+      pprint(api_response)
+      for i in api_response.items:
+        print("%s\t%s" %
+          (i.metadata.namespace, i.metadata.name))
+    except ApiException as e:
+      print("Exception when calling CoreV1Api->list_namespaced_limit_range: %s\n" % e)
+
     create_limitrange(kopf=kopf,name=name,spec=spec,logger=logger,api=api,filename='limitrange.yaml')
     
     api = kubernetes.client.NetworkingV1Api()
+    try: 
+      api_response = api.list_namespaced_network_policy(namespace=name) #, pretty=pretty, field_selector=field_selector, include_uninitialized=include_uninitialized, label_selector=label_selector, resource_version=resource_version, timeout_seconds=timeout_seconds, watch=watch)
+      pprint(api_response)
+      for i in api_response.items:
+        print("%s\t%s" %
+          (i.metadata.namespace, i.metadata.name))
+    except ApiException as e:
+      print("Exception when calling NetworkingV1Api->list_namespaced_network_policy: %s\n" % e)
+
     create_networkpolicy(kopf=kopf,name=name,spec=spec,logger=logger,api=api,filename='networkpolicy-allow-dns-access.yaml')
     create_networkpolicy(kopf=kopf,name=name,spec=spec,logger=logger,api=api,filename='networkpolicy-default-deny-egress.yaml')
     create_networkpolicy(kopf=kopf,name=name,spec=spec,logger=logger,api=api,filename='networkpolicy-default-deny-ingress.yaml')
 
     return {'limitrange-np-name': name} 
 
+# use env variable to control loop interval in seconds 
 LOOP_INTERVAL = int(os.environ['LOOP_INTERVAL'])
 @kopf.timer('namespace', interval=LOOP_INTERVAL,sharp=True)
 def check_object_on_time(spec, name, namespace, logger, **kwargs):
