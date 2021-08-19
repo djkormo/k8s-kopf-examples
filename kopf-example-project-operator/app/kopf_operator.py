c867a4285d75e20b1ef71864101a45769cd6c5bd
djkormo <djkormo@gmail.com>
2021-08-19 20:26:18 +0200

    adding resourcequota 1

--- kopf-example-project-operator/app/kopf_operator.py
+++ kopf-example-project-operator/app/kopf_operator.py
@@ -21,19 +21,7 @@ def get_random_value(**kwargs):
 
 
 # check if namespace should be under operator controll
-def check_namespace(name,excluded_namespaces):
-  env = Env()
-  env.read_env()  # read .env file, if it exists
-  namespace_list = env.list(excluded_namespaces)
-  if name in namespace_list:
-    print(f"Excluded namespace list: {namespace_list} ")    
-    print(f"Excluded namespace found: {name}")
-    return True
-  else:
-     return False  
 
-
-# check if namespace should be under operator controll
 def check_namespace(name,excluded_namespaces):
   env = Env()
   env.read_env()  # read .env file, if it exists
@@ -70,32 +58,52 @@ def create_namespace(kopf,name,namespace,spec,logger,api,filename):
 
 # replace namespace      
 def replace_namespace(kopf,name,namespace,spec,logger,api,filename):
+
   path = os.path.join(os.path.dirname(__file__), filename)
   tmpl = open(path, 'rt').read()
 
 # create resourcequota based on yaml manifest
 def create_resourcequota(kopf,name,spec,logger,api,filename):
+
   path = os.path.join(os.path.dirname(__file__), filename)
   tmpl = open(path, 'rt').read()
-  limitrangemaxcpu = spec.get('limitrangemaxcpu',"20000m")
-  limitrangemaxmem = spec.get('limitrangemaxmem',"30Gi")
-  limitrangemincpu = spec.get('limitrangemincpu',"50m")
-  limitrangeminmem = spec.get('limitrangeminmem',"50Mi")
-  limitrangedefaultcpu = spec.get('limitrangedefaultcpu',"1000m")
-  limitrangedefaultmem = spec.get('limitrangedefaultmem',"1000Mi")
-  limitrangedefaultrequestcpu = spec.get('limitrangedefaultrequestcpu',"100m")
-  limitrangedefaultrequestmem = spec.get('limitrangedefaultrequestmem',"100Mi")
 
-  text = tmpl.format(name=name,limitrangemaxmem=limitrangemaxmem,
-           limitrangemaxcpu=limitrangemaxcpu, 
-           limitrangemincpu=limitrangemincpu,
-           limitrangeminmem=limitrangeminmem,
-           limitrangedefaultcpu=limitrangedefaultcpu,
-           limitrangedefaultmem=limitrangedefaultmem,
-           limitrangedefaultrequestcpu=limitrangedefaultrequestcpu,
-           limitrangedefaultrequestmem=limitrangedefaultrequestmem,
+  resourcequotarequestscpu = spec.get('resourcequotarequestscpu',1)
+  if not resourcequotarequestscpu:
+    raise kopf.PermanentError(f"resourcequotarequestscpu must be set. Got {resourcequotarequestscpu!r}.")
+
+  resourcequotarequestsmemory = spec.get('resourcequotarequestsmemory',1)
+  if not resourcequotarequestsmemory:
+    raise kopf.PermanentError(f"resourcequotarequestsmemory must be set. Got {resourcequotarequestsmemory!r}.")
+    
+  resourcequotalimitscpu=spec.get('resourcequotalimitscpu',1)
+    
+  resourcequotalimitsmemory=spec.get('resourcequotalimitsmemory',1)
+  resourcequotacountjobsbatch=spec.get('resourcequotacountjobsbatch',1)
+  resourcequotacountingresses=spec.get('resourcequotacountingresses',1)
+  resourcequotapods=spec.get('resourcequotapods',1)
+  resourcequotaservices=spec.get('resourcequotaservices',1)
+  resourcequotaconfigmaps=spec.get('resourcequotaconfigmaps',1)
+  resourcequotapersistentvolumeclaims=spec.get('resourcequotapersistentvolumeclaims',1),
+  resourcequotareplicationcontrollers=spec.get('resourcequotareplicationcontrollers',1)
+  resourcequotasecrets=spec.get('resourcequotasecrets',1)
+  resourcequotaservicesloadbalancers=spec.get('resourcequotaservicesloadbalancers',1)
+
+  text = tmpl.format(name=name,resourcequotarequestscpu=resourcequotarequestscpu,
+           resourcequotarequestsmemory=resourcequotarequestsmemory, 
+           resourcequotalimitscpu=resourcequotalimitscpu,
+           resourcequotalimitsmemory=resourcequotalimitsmemory,
+           resourcequotacountjobsbatch=resourcequotacountjobsbatch,
+           resourcequotacountingresses=resourcequotacountingresses,
+           resourcequotapods=resourcequotapods,
+           resourcequotaservices=resourcequotaservices,
+           resourcequotaconfigmaps=resourcequotaconfigmaps,
+           resourcequotapersistentvolumeclaims=resourcequotapersistentvolumeclaims,
+           resourcequotareplicationcontrollers=resourcequotareplicationcontrollers,
+           resourcequotasecrets=resourcequotasecrets,
+           resourcequotaservicesloadbalancers=resourcequotaservicesloadbalancers
     )
-
+    
   data = yaml.safe_load(text)
   try:
     obj = api.create_namespaced_resource_quota(
@@ -103,34 +111,53 @@ def create_resourcequota(kopf,name,spec,logger,api,filename):
         body=data,
       )
     kopf.append_owner_reference(obj)
-    #logger.info(f"LimitRange child is created: {obj}")
+    #logger.info(f"ResourceQuota child is created: {obj}")
   except ApiException as e:
     print("Exception when calling CoreV1Api->create_namespaced_resource_quota: %s\n" % e)
   kopf.adopt(data)
 
 # replace resourcequota based on yaml manifest
 def replace_resourcequota(kopf,name,spec,logger,api,filename):
+
   path = os.path.join(os.path.dirname(__file__), filename)
   tmpl = open(path, 'rt').read()
-  limitrangemaxcpu = spec.get('limitrangemaxcpu',"20000m")
-  limitrangemaxmem = spec.get('limitrangemaxmem',"30Gi")
-  limitrangemincpu = spec.get('limitrangemincpu',"50m")
-  limitrangeminmem = spec.get('limitrangeminmem',"50Mi")
-  limitrangedefaultcpu = spec.get('limitrangedefaultcpu',"1000m")
-  limitrangedefaultmem = spec.get('limitrangedefaultmem',"1000Mi")
-  limitrangedefaultrequestcpu = spec.get('limitrangedefaultrequestcpu',"100m")
-  limitrangedefaultrequestmem = spec.get('limitrangedefaultrequestmem',"100Mi")
 
-  text = tmpl.format(name=name,limitrangemaxmem=limitrangemaxmem,
-           limitrangemaxcpu=limitrangemaxcpu, 
-           limitrangemincpu=limitrangemincpu,
-           limitrangeminmem=limitrangeminmem,
-           limitrangedefaultcpu=limitrangedefaultcpu,
-           limitrangedefaultmem=limitrangedefaultmem,
-           limitrangedefaultrequestcpu=limitrangedefaultrequestcpu,
-           limitrangedefaultrequestmem=limitrangedefaultrequestmem,
+  resourcequotarequestscpu = spec.get('resourcequotarequestscpu',1)
+  if not resourcequotarequestscpu:
+    raise kopf.PermanentError(f"resourcequotarequestscpu must be set. Got {resourcequotarequestscpu!r}.")
+
+  resourcequotarequestsmemory = spec.get('resourcequotarequestsmemory',1)
+  if not resourcequotarequestsmemory:
+    raise kopf.PermanentError(f"resourcequotarequestsmemory must be set. Got {resourcequotarequestsmemory!r}.")
+    
+  resourcequotalimitscpu=spec.get('resourcequotalimitscpu',1)
+    
+  resourcequotalimitsmemory=spec.get('resourcequotalimitsmemory',1)
+  resourcequotacountjobsbatch=spec.get('resourcequotacountjobsbatch',1)
+  resourcequotacountingresses=spec.get('resourcequotacountingresses',1)
+  resourcequotapods=spec.get('resourcequotapods',1)
+  resourcequotaservices=spec.get('resourcequotaservices',1)
+  resourcequotaconfigmaps=spec.get('resourcequotaconfigmaps',1)
+  resourcequotapersistentvolumeclaims=spec.get('resourcequotapersistentvolumeclaims',1),
+  resourcequotareplicationcontrollers=spec.get('resourcequotareplicationcontrollers',1)
+  resourcequotasecrets=spec.get('resourcequotasecrets',1)
+  resourcequotaservicesloadbalancers=spec.get('resourcequotaservicesloadbalancers',1)
+
+  text = tmpl.format(name=name,resourcequotarequestscpu=resourcequotarequestscpu,
+           resourcequotarequestsmemory=resourcequotarequestsmemory, 
+           resourcequotalimitscpu=resourcequotalimitscpu,
+           resourcequotalimitsmemory=resourcequotalimitsmemory,
+           resourcequotacountjobsbatch=resourcequotacountjobsbatch,
+           resourcequotacountingresses=resourcequotacountingresses,
+           resourcequotapods=resourcequotapods,
+           resourcequotaservices=resourcequotaservices,
+           resourcequotaconfigmaps=resourcequotaconfigmaps,
+           resourcequotapersistentvolumeclaims=resourcequotapersistentvolumeclaims,
+           resourcequotareplicationcontrollers=resourcequotareplicationcontrollers,
+           resourcequotasecrets=resourcequotasecrets,
+           resourcequotaservicesloadbalancers=resourcequotaservicesloadbalancers
     )
-
+    
   data = yaml.safe_load(text)
   try:
     obj = api.replace_namespaced_resource_quota(
@@ -139,18 +166,18 @@ def replace_resourcequota(kopf,name,spec,logger,api,filename):
         body=data,
       )
     kopf.append_owner_reference(obj)
-    #logger.info(f"LimitRange child is created: {obj}")
+    #logger.info(f"ResurceQuota child is replaced: {obj}")
   except ApiException as e:
     print("Exception when calling CoreV1Api->replace_namespaced_resource_quota: %s\n" % e)
   kopf.adopt(data)  
   
 
 
-
 # When creating or resuming object
 @kopf.on.resume('djkormo.github', 'v1alpha1', 'project')
 @kopf.on.create('djkormo.github', 'v1alpha1', 'project')
 def create_fn(spec, name, namespace, logger, **kwargs):
+    
     print(f"Creating: {spec}")
     api = kubernetes.client.CoreV1Api()
     
@@ -180,6 +207,28 @@ def create_fn(spec, name, namespace, logger, **kwargs):
       replace_namespace(kopf=kopf,name=name,namespace=namespace,spec=spec,logger=logger,api=api,filename='namespace.yaml')
     
 
+    # create resource quota
+    
+    api = kubernetes.client.CoreV1Api()
+
+    try: 
+      api_response = api.list_namespaced_resource_quota(namespace=name) 
+      #pprint(api_response)
+      l_resoucequota=[]
+      for i in api_response.items:
+        print("ResourceQuota namespace: %s\t name: %s" %
+          (i.metadata.namespace, i.metadata.name))
+        l_resoucequota.append(i.metadata.name)
+    except ApiException as e:
+      print("Exception when calling CoreV1Api->list_namespaced_resource_quota: %s\n" % e)
+
+    if name not in l_resoucequota:
+      create_resourcequota(kopf=kopf,name=name,spec=spec,logger=logger,api=api,filename='resourcequota.yaml')
+    else:
+      replace_resourcequota(kopf=kopf,name=name,spec=spec,logger=logger,api=api,filename='limitrange.yaml')
+ 
+    
+
 # When updating object
 @kopf.on.update('djkormo.github', 'v1alpha1', 'project')
 def update_fn(spec, name, status, namespace, logger,diff, **kwargs):
@@ -202,11 +251,14 @@ def update_fn(spec, name, status, namespace, logger,diff, **kwargs):
     except ApiException as e:
       print("Exception when calling CoreV1Api->list_namespace: %s\n" % e)
 
+    # create or update namespace
     if name not in l_namespace:
       create_namespace(kopf=kopf,name=name,namespace=namespace,spec=spec,logger=logger,api=api,filename='namespace.yaml')
     else:
       replace_namespace(kopf=kopf,name=name,namespace=namespace,spec=spec,logger=logger,api=api,filename='namespace.yaml')
     
+   # create or update resourcequota TODO
+
 LOOP_INTERVAL = int(os.environ['LOOP_INTERVAL'])
 @kopf.on.timer('djkormo.github', 'v1alpha1', 'project',interval=LOOP_INTERVAL,sharp=True)
 def check_object_on_time(spec, name, namespace, logger, **kwargs):
