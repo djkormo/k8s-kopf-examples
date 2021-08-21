c11735664491a1408a45a83e47d49038779ef22a
djkormo <djkormo@gmail.com>
2021-08-21 21:36:22 +0200

    bash script 6

--- kopf-example-project-operator/app/operator.bash
+++ kopf-example-project-operator/app/operator.bash
@@ -1,20 +1,20 @@
 #!/bin/bash
 set -e
-#set -u
+set -u
 file="/home/worker/app/kopf_operator.py"
-command="/home/worker/.local/bin/kopf run --standalone --liveness=http://0.0.0.0:8080/healthz /home/worker/app/kopf_operator.py "
+command="/home/worker/.local/bin/kopf run --standalone "
 #[[ "$VERBOSE" = "true" ]] && command+=("--verbose")
 
 if [[ "$VERBOSE" -eq "true" ]]; then
     echo "VERBOSE is set true"
-    command= "$command --verbose"
+    command= "${command} --verbose"
 fi
 
 # [[ "$DEBUG" = "true" ]] && command+=("--debug")
 
 if [[ "$DEBUG" -eq "true" ]]; then
     echo "DEBUG is set true"
-    command= "$command --debug"
+    command= "${command} --debug"
 fi
 
 #[ -n "$NAMESPACE" ] && [ "$NAMESPACE" != "ALL" ] && echo "Only watching resources from the ${NAMESPACE} namespace" && command+=("--namespace=${NAMESPACE}")
@@ -23,8 +23,11 @@ fi
 
 if [[ "$LIVENESS" -eq "true" ]]; then
     echo "LIVENESS is set true"
-    # command= "$command --liveness=http://0.0.0.0:8080/healthz"
+   command= "${command} --liveness=http://0.0.0.0:8080/healthz"
 fi
+
+# add the name of operator file
+command= "${command} ${file}"
 
 USER=$(id -u)
 echo "Setting USER environment variable to ${USER}"
