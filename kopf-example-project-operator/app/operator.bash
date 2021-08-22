d491f41c18b342b424dc9b9fbf2b6343b1b2983e
djkormo <djkormo@gmail.com>
2021-08-22 15:59:33 +0200

    bash script 19

--- kopf-example-project-operator/app/operator.bash
+++ kopf-example-project-operator/app/operator.bash
@@ -6,26 +6,33 @@ command="/home/worker/.local/bin/kopf run  --standalone "
 cli_parameters=""
 #[[ "$VERBOSE" = "true" ]] && command+=("--verbose")
 
-if [[ "$VERBOSE" -eq "true" ]]; then
+if [[ $VERBOSE = "true" ]]; then
     echo "VERBOSE is set true"
-    #command= "${command}  --verbose"
-    cli_parameters+=(" --verbose")
+    cli_parameters="${cli_parameters} --verbose"
 fi
 
-if [[ "$DEBUG" -eq "true" ]]; then
+if [[ $DEBUG = "true" ]]; then
     echo "DEBUG is set true"
-    cli=" --debug "
-    cli_parameters+=(" --debug") 
+    cli_parameters="${cli_parameters} --debug"
 fi
 
-#[ -n "$NAMESPACE" ] && [ "$NAMESPACE" != "ALL" ] && echo "Only watching resources from the ${NAMESPACE} namespace" && command+=("--namespace=${NAMESPACE}")
+if [ -z "$NAMESPACE" ]; then 
+  echo "NAMESPACE is NULL"
+  cli_parameters="${cli_parameters} -A"
+  else 
+  echo "NAMESPACE is Not NULL"
+  if [[ $NAMESPACE != "ALL" ]] ; then
+    echo "NAMESPACE is set to ${NAMESPACE}"
+    cli_parameters="${cli_parameters} --namespace ${NAMESPACE}"
+  else
+    cli_parameters="${cli_parameters} -A"
+  fi
 
-#[[ "$LIVENESS" = "true" ]] && echo "Liveness /healthz endpoint has been explicitely enabled!" || command+=("--liveness=http://0.0.0.0:8080/healthz")
+fi
 
-if [[ "$LIVENESS" -eq "true" ]]; then
+if [[ $LIVENESS = "true" ]]; then
     echo "LIVENESS is set true"
-    cli=" --liveness=http://0.0.0.0:8080/healthz "
-    cli_parameters+=(" --liveness=http://0.0.0.0:8080/healthz") 
+    cli_parameters="${cli_parameters} ----liveness=http://0.0.0.0:8080/healthz"
 fi
 
 echo "Parameters: ${cli_parameters}"
@@ -37,4 +44,4 @@ USER=$(id -u)
 echo "Setting USER environment variable to ${USER}"
 export USER=$USER
 echo " Executing ${command}"
-eval "${command}"
+#eval "${command}"
