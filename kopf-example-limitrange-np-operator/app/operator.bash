443733f5aa63ea0feb03834e8827309ca02239b8
djkormo <djkormo@gmail.com>
2021-08-22 21:36:32 +0200

    refactoring

--- kopf-example-limitrange-np-operator/app/operator.bash
+++ kopf-example-limitrange-np-operator/app/operator.bash
@@ -1,32 +1,46 @@
 #!/bin/bash
 set -e
-#set -u
-file="/home/worker/app/operator.py"
-command="kopf run --standalone --verbose --debug --liveness=http://0.0.0.0:8080/healthz /home/worker/app/operator.py"
-#[[ "$VERBOSE" = "true" ]] && command+=("--verbose")
+file="/home/worker/app/kopf_operator.py"
+command="/home/worker/.local/bin/kopf run  --standalone "
+cli_parameters=""
 
-if [[ "$VERBOSE" -eq "true" ]]; then
+if [[ $VERBOSE = "true" ]]; then
     echo "VERBOSE is set true"
-    #command= "$command --verbose"
+    cli_parameters="${cli_parameters} --verbose"
+fi
+
+if [[ $DEBUG = "true" ]]; then
+    echo "DEBUG is set true"
+    cli_parameters="${cli_parameters} --debug"
+fi
+
+if [ -z "$NAMESPACE" ]; then 
+  echo "NAMESPACE is NULL"
+  cli_parameters="${cli_parameters} -A"
+  else 
+  echo "NAMESPACE is set to ${NAMESPACE} "
+  if [[ $NAMESPACE != "ALL" ]] ; then
+    echo "NAMESPACE is set to ${NAMESPACE}"
+    cli_parameters="${cli_parameters} --namespace ${NAMESPACE}"
+  else
+    cli_parameters="${cli_parameters} -A"
   fi
 
-#[[ "$DEBUG" = "true" ]] && command+=("--debug")
-if [[ "$DEBUG" -eq "true" ]]; then
-    echo "DEBUG is set true"
-    #command= "$command --debug"
 fi
 
-#[ -n "$NAMESPACE" ] && [ "$NAMESPACE" != "ALL" ] && echo "Only watching resources from the ${NAMESPACE} namespace" && command+=("--namespace=${NAMESPACE}")
-
-#[[ "$LIVENESS" = "true" ]] && echo "Liveness /healthz endpoint has been explicitely enabled!" || command+=("--liveness=http://0.0.0.0:8080/healthz")
-
-if [[ "$LIVENESS" -eq "true" ]]; then
+if [[ $LIVENESS = "true" ]]; then
     echo "LIVENESS is set true"
-    # command= "$command --liveness=http://0.0.0.0:8080/healthz"
+    cli_parameters="${cli_parameters} --liveness=http://0.0.0.0:8080/healthz"
 fi
+
+echo "Parameters: ${cli_parameters}"
+
+# add parameters and the name of operator file
+
+command="${command} ${cli_parameters} ${file}"
 
 USER=$(id -u)
 echo "Setting USER environment variable to ${USER}"
 export USER=$USER
 echo " Executing ${command}"
-exec "${command}"
+eval "${command}"
