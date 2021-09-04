d79fce52fabdf0af10a7798593d1565febee340f
djkormo <djkormo@gmail.com>
2021-09-04 20:49:44 +0200

    addling pod count and list

--- kopf-example-chaos-operator/app/kopf_operator.py
+++ kopf-example-chaos-operator/app/kopf_operator.py
@@ -8,6 +8,7 @@ from kubernetes.client.rest import ApiException
 from pprint import pprint
 import datetime
 import random
+import json
 
 # for Kubernetes probes
 
@@ -33,11 +34,67 @@ def check_namespace(name,excluded_namespaces):
   else:
      return False  
 
+# count all pods in namespace
+def count_pods(kopf,api,namespace,logger):
+    
+    api_response = api.list_namespaced_pod(namespace)
+    logger.info("Connected to Kubernetes host: %s", v1.api_client.configuration.host)
+    POD_COUNT = len(api_response.items)
+    logger.info("Number of pods: %s", POD_COUNT)
+
+    return(POD_COUNT)  
+
+    pod_list = v1.list_namespaced_pod("example")
+    for pod in pod_list.items:
+        print("%s\t%s\t%s" % (pod.metadata.name,
+                              pod.status.phase,
+                              pod.status.pod_ip))   
+
+def list_pods(kopf,api,namespace,logger):
+    if  False: #constants.OFFLINE_MODE == True:
+        return([0],[0])
+    else:
+        try:
+            ret = api.list_namespaced_pod(namespace)
+            POD_COUNT = len(ret.items)
+            logger.info("Number of pods: %s", POD_COUNT)
+
+            # Select random pod
+            # print(ret.items[0].metadata.namespace)
+            random.shuffle(ret.items)
+            #while ret.items[0].metadata.namespace in constants.EXCLUDES_LIST:
+            #    logger.info("Pod in excluded namespace, shuffling")
+            #    random.shuffle(ret.items)
+            POD_TO_KILL = ret.items[0].metadata.name
+            POD_NAMESPACE = ret.items[0].metadata.namespace
+            return([POD_TO_KILL, POD_NAMESPACE])
+        except Exception as e:
+            logger.error("Unable to list pods: %s", (e))
+            return([0],[0])
+
+
 # When creating or resuming object
 @kopf.on.resume('djkormo.github', 'v1alpha1', 'chaos')
 @kopf.on.create('djkormo.github', 'v1alpha1', 'chaos')
 def create_fn(spec, name, namespace, logger, **kwargs):
     print(f"Creating: {spec}")
+    
+    # check for excluded namespace
+
+    if check_namespace(name=name,excluded_namespaces='EXCLUDED_NAMESPACES'):
+      return {'chaos-operator-name': namespace}    
+
+    # count pods in our namespace
+    
+    api = kubernetes.client.CoreV1Api()
+
+    pod_count=count_pods(kopf=kopf,api=api,namespace=name,logger=logger)
+    print(f"The are: {pod_count} pods in {name} namespace")
+    # choose one pod to delete
+    if pod_count>0:
+      [pod_name,pod_namespace] = list_pods(kopf=kopf,api=api,namespace=name,logger=logger)
+    ## TODO  delete pod 
+     
 
 LOOP_INTERVAL = int(os.environ['LOOP_INTERVAL'])
 @kopf.on.timer('djkormo.github', 'v1alpha1', 'chaos',interval=LOOP_INTERVAL,sharp=True)
