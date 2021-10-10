0d30e4780b4dd37128377ef7e226941f960d2cea
djkormo <djkormo@gmail.com>
2021-10-10 21:27:56 +0200

    operator_pod=pod/shutdown-operator-64c4b4f8f-xj5rh

--- kopf-example-shutdown-operator/app/kopf_operator.py
+++ kopf-example-shutdown-operator/app/kopf_operator.py
@@ -32,8 +32,8 @@ def turn_off_deployment(name,namespace,logger,kopf,metadata,spec,api,dry_run):
   logger.info("Turning off Deployment %s in namespace %s", name,namespace)
   
   # how many replicas we have
-  replicas = spec.get('replicas')  
-
+  replicas = spec['replicas']
+  
   logger.info("Deployment %s in %s namespace has %s replicas", name,namespace,replicas)
   
   # save replicas and timestamp to proper annotations
