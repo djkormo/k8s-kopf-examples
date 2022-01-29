b106f2359b88ba87a15aadd02aae24627688388b
djkormo <djkormo@gmail.com>
2022-01-29 20:02:53 +0100

    adding labels mock 10

--- kopf-example-project-operator/app/kopf_operator.py
+++ kopf-example-project-operator/app/kopf_operator.py
@@ -99,6 +99,8 @@ def replace_namespace(kopf,name,namespace,meta,spec,logger,api,filename):
   #labels = {"owner": "djkormo", "name": "project"}
   annotations = {"description": "test","confirmation":"yes"}
   
+  logger.info(f"Project LABELS {labels} \n {meta.labels} \n")
+  
   # Apply annotations to namespace
   try:
     obj = api.patch_namespace(
